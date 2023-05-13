9e9e71a1e5898d797033d59ed1402bebdfa520ef
MIchaelDoan <michael.doan18@gmail.com>
2023-05-13 20:35:13 +1000

    Grid working 100%

--- code.js
+++ code.js
@@ -2,10 +2,18 @@ const container = document.querySelector('#container');
 
 for(let i = 0; i < 16 ; i++){
     const row = document.createElement('div');
+    row.classList.add('row');
     container.appendChild(row);
     for(let j = 0; j < 16; j++){
-    const column = document.createElement('div');
-    row.appendChild(column);
+    const gridCell = document.createElement('div');
+    gridCell.setAttribute('id', i + "+" + j);
+    gridCell.classList.add('gridCell');
+    row.appendChild(gridCell);
+    
     }
     
 }
+
+/*const cRow = document.createElement('p');
+cRow.textContent = "C" + i;
+row.appendChild(cRow);*/
